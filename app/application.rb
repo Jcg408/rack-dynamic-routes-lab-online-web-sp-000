66daa60ed03d133e0d00f5a0cd5fe721d7a59857
Jessica Gladstone <jcg321@comcast.net>
2019-02-04 17:02:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,15 @@ class Application
     if req.path=="/items/"
       item_name = req.path.split("/items/").last
       
-      item = @@items.find{|i| i.name == ite_name}
-      
-      
-      
-    end
+      item = @@items.find{|i| i.name == item_name}
+        if item
+          resp.write "#{item.price}"
+        else
+          resp.write "Item not found"
+          resp.status = 404
+        end
+        
+        
  
  
  
