6a4d94d2505e790556dc87fc33c216ffa16c5f01
Jessica Gladstone <jcg321@comcast.net>
2019-02-04 16:53:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path=="/items/"
-      item = req.path.split("/items/").last
+      item_name = req.path.split("/items/").last
       
       
       
