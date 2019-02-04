2d8b40fa6f4ab3b7cda8ac9afd93e5acaaed56fa
Jessica Gladstone <jcg321@comcast.net>
2019-02-04 17:17:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path=="/items/"
-      item_name = req.path.split(/items/).last
+    if req.path==(/items/)
+      item_name = req.path.split("/items/").last
       
       item = @@items.find{|i| i.name == item_name}
         if item
